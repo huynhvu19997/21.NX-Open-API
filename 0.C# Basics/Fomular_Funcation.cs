aad81abaacaf374faee0228fd1e4d5818dbf0996
Vu Huynh Nguyen Hoang <46526505+huynhvu19997@users.noreply.github.com>
2025-10-13 13:30:44 +0700

    Create Fomular_Funcation.cs

---
+++ 0.C# Basics/Fomular_Funcation.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fomular_match
+{
+    class Fomular_Funcation
+    {
+        public double cong( double a, double b)
+        {
+            return a + b;
+        }
+
+        public void tru()
+        {
+            abc();
+        }
+
+        public void Nhan()
+        {
+
+        }
+
+        public void chia()
+        {
+
+        }
+
+        public string inGiaTri( string giatrinhapvao)
+        {
+            return giatrinhapvao;
+        }
+
+        private void abc()
+        {
+
+        }
+    }
+}
