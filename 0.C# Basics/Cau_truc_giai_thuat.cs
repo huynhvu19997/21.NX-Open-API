85665a385ea7ae215be6687ac73631355e2b3e4d
Vu Huynh Nguyen Hoang <46526505+huynhvu19997@users.noreply.github.com>
2025-10-15 10:55:16 +0700

    Add files via upload

---
+++ 0.C# Basics/Cau_truc_giai_thuat.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cau_truc_giai_thuat
+{
+    class Cau_truc_giai_thuat
+    {
+        /*
+         Tutorial Content
+            CẤU TRÚC DỮ LIỆU
+
+                Các kiểu tập hợp (Collection) trong C#: khái niệm, phân loại
+                Mảng trong C#: một chiều, nhiều chiều, răng cưa (jagged)
+                Danh sach (list) trong C#: ArrayList, List, SortedList
+                Danh sách liên kết (linked list) trong C#
+                Ngăn xếp (stack) trong C#: cài đặt, ứng dụng, lớp Stack
+                Hàng đợi (queue) trong C#: cài đặt, ứng dụng, lớp Queue
+                Bảng băm (Hashtable) và từ điển (Dictionary) trong C# .NET
+            THUẬT TOÁN
+
+                Đánh giá thuật toán - đo thời gian thực thi code trong C#
+                Các thuật toán sắp xếp với C#: sắp xếp chọn, chèn, nổi bọt, nhanh
+                Các thuật toán tìm kiem cơ bản: tìm kiếm tuan tự, tìm kiếm nhị phân
+         */
+    }
+}
